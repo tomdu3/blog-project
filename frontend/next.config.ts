dfb481a6cae07f7f8211027475d6bc2044c51483
Tomislav Dukez <tomdu3@ymail.com>
2025-07-06 17:39:13 +0100

    update frontend settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['s3.us-west-2.amazonaws.com', 'prod-files-secure.s3.us-west-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
