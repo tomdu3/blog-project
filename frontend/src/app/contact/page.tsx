529d35ef5f860dc3c4848868779ce840245891cb
Tomislav Dukez <tomdu3@ymail.com>
2025-08-26 21:58:28 +0100

    fix: apostrophes in typescript nextjs

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -5,7 +5,7 @@ const ContactPage = () => {
     <div className="max-w-4xl mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
       <p className="text-lg text-gray-700 leading-relaxed mb-4">
-        I'm always open to discussing new projects, creative ideas, or opportunities to be part of an amazing team. Feel free to reach out to me through any of the following channels:
+        I&apos;m always open to discussing new projects, creative ideas, or opportunities to be part of an amazing team. Feel free to reach out to me through any of the following channels:
       </p>
       <ul className="list-disc list-inside text-lg text-gray-700 leading-relaxed">
         <li>Email: [your-email@example.com](mailto:your-email@example.com)</li>
