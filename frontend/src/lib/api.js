539a1054774aff50afc20d5bc736186dac265303
Tomislav Dukez <tomdu3@ymail.com>
2025-09-04 19:38:23 +0100

    update: getPost with slug

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -21,3 +21,43 @@ export async function getPosts() {
     return [];
   }
 }
+
+
+export async function getPost(slug) {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  
+  try {
+    const res = await fetch(`${apiUrl}/posts/${slug}`, {
+      next: { revalidate: 600 }
+    });
+    
+    if (!res.ok) {
+      console.error(`Failed to fetch post with slug: ${slug}`);
+      return null;
+    }
+    
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(`Error fetching post with slug ${slug}:`, error);
+    return null;
+  }
+}
+
+export async function sendContactForm(data) {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    const res = await fetch(`${apiUrl}/contact`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to send message');
+    }
+
+    return res.json();
+}
