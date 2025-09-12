73f3a56c2dfb92361d3c7e833c160cdbfb7c1ef2
Tomislav Dukez <tomdu3@ymail.com>
2025-09-12 19:34:24 +0100

    add images for the articles list cover or placeholder

--- frontend/src/components/ArticleList.jsx
+++ frontend/src/components/ArticleList.jsx
@@ -8,17 +8,26 @@ const ArticleList = ({ posts }) => {
       <h2 className="text-lg font-medium text-slate-300 mb-4">All Articles</h2>
       <ul>
         {posts.map((post) => (
-          <li key={post.id} className="mb-4">
-            <Link href={`/blog/${post.slug}`} className="text-slate-200 hover:text-white hover:underline font-medium">
-              {post.title}
-            </Link>
-            <p className="text-sm text-slate-400 mt-1">
-              {new Date(post.date).toLocaleDateString('en-US', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-              })}
-            </p>
+          <li key={post.id} className="mb-4 flex items-center">
+            <div className="w-20 h-20 mr-4 flex-shrink-0">
+              <img 
+                src={post.cover ? post.cover : '/town-crier.webp'} 
+                alt={post.title} 
+                className="w-full h-full object-cover rounded-lg" 
+              />
+            </div>
+            <div>
+              <Link href={`/blog/${post.slug}`} className="text-slate-200 hover:text-white hover:underline font-medium">
+                {post.title}
+              </Link>
+              <p className="text-sm text-slate-400 mt-1">
+                {new Date(post.date).toLocaleDateString('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric'
+                })}
+              </p>
+            </div>
           </li>
         ))}
       </ul>
