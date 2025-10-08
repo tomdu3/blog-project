d007898072d9b37586eec3c4fc5eed1e33075d3a
Tomislav Dukez <tomdu3@ymail.com>
2025-10-08 16:35:03 +0100

    update ArticleList component for posts page link

--- frontend/src/components/ArticleList.jsx
+++ frontend/src/components/ArticleList.jsx
@@ -1,18 +1,23 @@
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 const ArticleList = ({ posts }) => {
   console.log('ArticleList received posts:', posts);
+  const displayedPosts = posts.slice(0, 10);
+
   return (
         <aside className="p-6 bg-slate-800 rounded-lg pb-8">
       <h2 className="text-lg font-medium text-slate-300 mb-4">All Articles</h2>
       <ul>
-        {posts.map((post) => (
+        {displayedPosts.map((post) => (
           <li key={post.id} className="mb-4 flex items-center">
             <div className="w-20 h-20 mr-4 flex-shrink-0">
-              <img 
+              <Image 
                 src={post.cover ? post.cover : '/town-crier.webp'} 
                 alt={post.title} 
+                width={80}
+                height={80}
                 className="w-full h-full object-cover rounded-lg" 
               />
             </div>
@@ -31,6 +36,11 @@ const ArticleList = ({ posts }) => {
           </li>
         ))}
       </ul>
+      <div className="mt-4 text-center">
+        <Link href="/posts" className="text-slate-200 hover:text-white hover:underline font-medium">
+          Posts
+        </Link>
+      </div>
     </aside>
   );
 }
